057589223c6620fc1a2b039dc7e1d9bea2e09ade
Gil Yasur <gilyasur@MacBook-Pro-3.local>
2025-04-13 23:40:28 +0300

    favicon upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ import { SectionProvider } from '@/context/SectionContext';
 export const metadata: Metadata = {
   title: "Roy Peker - Senior Digital Compositor",
   description: "A portfolio for Roy Peker, a senior digital compositor with a passion for creating stunning visual effects.",
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ]
+  }
 };
 
 export default function RootLayout({
